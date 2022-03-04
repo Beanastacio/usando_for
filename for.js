8a1deb28b86b6538d09a04d049cc380fb379f109
unknown <beatrizanastacios@gmail.com>
2022-03-04 17:24:12 -0300

    meus arquivos

---
+++ for.js
@@ -0,0 +1,475 @@
+// Criando o Array
+var jsonCardapioEntradas = {
+  entradas: [
+    {
+      id: "",
+      anTitulo: "TIROKAFTERI",
+      anDescricao:
+        "pasta de pimentão vermelho assado e queijo feta · acompanham pão pita e pão de fermentação natural`n<em >[possui versão vegana]</em>",
+      anPreco: "26",
+      anFoto: "https://www.instagram.com/p/CQw6XacpLMu/",
+      boVegetariano: 1,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "SALADA SELVAGEM",
+      anDescricao:
+        "rúcula selvagem · figo ·  vinagrete de mel · pistache`n<em >[possui versão vegana]</em>",
+      anPreco: "28",
+      anFoto: "https://www.instagram.com/p/CP0z8mppUWd/",
+      boVegetariano: 1,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "SALADA GREGA",
+      anDescricao:
+        "queijo de cabra · tomate cereja · pepino · cebola roxa · alcaparra · azeitona Calamata · orégano`n<em >[possui versão vegana]</em>",
+      anPreco: "30",
+      anFoto: "https://www.instagram.com/p/CTfFJ14rfr9/?utm_medium=copy_link",
+      boVegetariano: 1,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "MELITZANA",
+      anDescricao:
+        "berinjela grelhada com especiarias · iogurte grego · tomate confitado · cebola roxa · mel · hortelã`n<em >[possui versão vegana e sem glúten]</em>",
+      anPreco: "34",
+      anFoto: "https://www.instagram.com/p/CRPm9KGg8yb/",
+      boVegetariano: 1,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "CROQUETA DE COSTELA (3 un)",
+      anDescricao:
+        "croqueta cremosa de costela bovina · acompanha emulsão de agrião",
+      anPreco: "27",
+      anFoto: "https://www.instagram.com/p/CXUPvRaJ94E/",
+      boVegetariano: 0,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "CROQUETA DE FONTINA (2 un)",
+      anDescricao:
+        "croqueta cremosa de queijo Fontina · acompanha compota de tomate",
+      anPreco: "20",
+      anFoto:
+        "https://www.instagram.com/reel/CTFP9T5D7zI/?utm_medium=copy_link",
+      boVegetariano: 1,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "ROBALO CURADO`n[consulte disponibilidade]",
+      anDescricao:
+        "robalo curado com páprica · maçã-verde · limão siciliano · gengibre · flor de coentro",
+      anPreco: "38",
+      anFoto: "https://www.instagram.com/p/CPbnEDVpIwO/",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "CRUDO DE ATUM",
+      anDescricao: "atum · iogurte grego · pepino · azeite de endro · tapenade",
+      anPreco: "52",
+      anFoto: "https://www.instagram.com/p/CRaFVx2plHV/?utm_medium=copy_link",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "POLVO GRELHADO",
+      anDescricao:
+        "tentáculos de polvo grelhados · creme de amêndoas · salsa verde`n<em >[contém glúten]</em>",
+      anPreco: "89",
+      anFoto: "https://www.instagram.com/p/CYXQDdcJj6u/?utm_medium=copy_link",
+      boVegetariano: 0,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "BAO DE CAMARÃO",
+      anDescricao:
+        "pão de nori no vapor · camarão · molho levemente picante · acelga · coentro",
+      anPreco: "29",
+      anFoto: "https://www.instagram.com/p/CY6vreOLQXU/",
+      boVegetariano: 0,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "SANDUÍCHE DE LULA (2 un)",
+      anDescricao:
+        "brioche de tinta de lula · lula empanada · tomate confitado · cebola roxa · aioli",
+      anPreco: "34",
+      anFoto: "https://www.instagram.com/p/CZNqJ3xJULU/",
+      boVegetariano: 0,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "SUVLAKI DE CORDEIRO",
+      anDescricao:
+        "pão pita · cordeiro · molho de páprica · cebola roxa · hortelã",
+      anPreco: "24",
+      anFoto: "https://www.instagram.com/p/CTSUOt4rViO/",
+      boVegetariano: 0,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "SUVLAKI DE COGUMELOS",
+      anDescricao:
+        "pão pita · cogumelo eryngui · cogumelo portobello · molho de iogurte`n<em >[possui versão vegana]</em>",
+      anPreco: "24",
+      anFoto: "https://www.instagram.com/p/CRUM2P2JBt8/",
+      boVegetariano: 1,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "OSTRA FRESCA (2 un)`n[consulte disponibilidade]",
+      anDescricao:
+        "– ostra · limão siciliano · sal Maldon`n– ostra · granita de morango · endro",
+      anPreco: "22",
+      anFoto: "https://www.instagram.com/p/CSSkdorJqpn/",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "OSTRA FRESCA (6 un)`n[consulte disponibilidade]",
+      anDescricao:
+        "– ostra · limão siciliano · sal Maldon`n– ostra · granita de morango · endro",
+      anPreco: "59",
+      anFoto: "https://www.instagram.com/p/CSu_ZSrs0yQ/",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "PEIXE MARINADO",
+      anDescricao:
+        "manjubinha crua marinada em vinagre de Jerez · pepino · endro",
+      anPreco: "26",
+      anFoto: "https://www.instagram.com/p/CPvkDG-pUV7/",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 1,
+    },
+    {
+      id: "",
+      anTitulo:
+        "CROQUETA DO MAR (2 un)`n<em >[alérgicos: contém crustáceo]</em>",
+      anDescricao:
+        "croqueta cremosa de polvo e camarão · acompanha emulsão de pimenta",
+      anPreco: "32",
+      anFoto: "https://www.instagram.com/p/CadNy2spRJu/",
+      boVegetariano: 0,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+  ],
+  pratosPrincipais: [
+    {
+      id: "",
+      anTitulo: "COSTELA DESOSSADA",
+      anDescricao:
+        "costela bovina desossada · purê de cenoura assada · agrião · redução de caldo bovino",
+      anPreco: "79",
+      anFoto: "https://www.instagram.com/p/CS9uOnxL-py/",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+    {
+      id: "1",
+      anTitulo: "PSÁRI",
+      anDescricao:
+        "peixe do dia grelhado · purê de grão-de-bico · kale tostada · vinagrete de mel`n<em >[possui versão vegana]</em>",
+      anPreco: "72",
+      anFoto: "https://www.instagram.com/p/CQg3BxLpSOy/",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+    {
+      id: "2",
+      anTitulo: "FALSA LASANHA",
+      anDescricao:
+        "lâminas de batata · espinafre · queijo de cabra · molho bechamel · kale tostada`n<em >[possui versão vegana]</em>",
+      anPreco: "62",
+      anFoto: "https://www.instagram.com/p/CW8izuNJJia/",
+      boVegetariano: 1,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "ARROZ DO MAR",
+      anDescricao: "miniarroz · camarão · polvo · lula · aioli",
+      anPreco: "75",
+      anFoto: "https://www.instagram.com/p/CLz_N2bpvwx/",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "ARROZ NEGRO COM POLVO",
+      anDescricao:
+        "miniarroz com tinta de lula · polvo grelhado · emulsão de agrião`n<em >[alérgicos: contém caldo de camarão]</em>",
+      anPreco: "89",
+      anFoto: "",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+
+    {
+      id: "",
+      anTitulo: "ARROZ DE COGUMELOS",
+      anDescricao:
+        "miniarroz · cogumelo eryngui · cogumelo portobello · queijo de cabra`n<em >[possui versão vegana]</em>",
+      anPreco: "65",
+      anFoto: "https://www.instagram.com/p/CSaNxQXJweW/",
+      boVegetariano: 1,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "ARROZ FRITO DE CORDEIRO",
+      anDescricao:
+        "miniarroz tostado · paleta de cordeiro desfiada · gema de ovo curada · acelga tostada · iogurte grego · hortelã",
+      anPreco: "83",
+      anFoto: "https://www.instagram.com/p/CXhdUFXMpMT/",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+
+    {
+      id: "",
+      anTitulo: "GNOCCHI CROCANTE",
+      anDescricao:
+        "gnocchi de batata grelhado · creme de parmesão · pangrattato`n<em >[possui versão vegana]</em>",
+      anPreco: "62",
+      anFoto: "https://www.instagram.com/p/CYCCzOaL1jv/",
+      boVegetariano: 1,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+
+    {
+      id: "",
+      anTitulo: "FLAT IRON",
+      anDescricao:
+        "corte bovino · mil folhas de batata · kale tostada · redução de caldo bovino",
+      anPreco: "80",
+      anFoto: "https://www.instagram.com/p/CKw1rzhpsYY/",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+
+    {
+      id: "",
+      anTitulo: "GNOCCHI DE  BATATA DOCE ROXA",
+      anDescricao:
+        "gnocchi de batata doce roxa · creme de parmesão · queijo de cabra · nozes · mel",
+      anPreco: "62",
+      anFoto: "https://www.instagram.com/p/CUqVjhNpyx8/",
+      boVegetariano: 1,
+      boSemGluten: 0,
+      boInactive: 1,
+    },
+
+    {
+      id: "",
+      anTitulo: "PRATO KIDS",
+      anDescricao:
+        "macarrão ao sugo ou na manteiga · filé mignon · chips de batata ou kale tostada",
+      anPreco: "36",
+      anFoto: "",
+      boVegetariano: 0,
+      boSemGluten: 0,
+      boInactive: 1,
+    },
+  ],
+  sobremesas: [
+    {
+      id: "",
+      anTitulo: "BAKLAVA DE PISTACHE",
+      anDescricao: "lâminas de massa filo · creme de pistache",
+      anPreco: "32",
+      anFoto: "https://www.instagram.com/p/CPqR_zcJUtq/",
+      boVegetariano: 1,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: 1,
+      anTitulo: "TORTA DE CHOCOLATE",
+      anDescricao:
+        "torta de chocolate meio amargo · caramelo salgado`n<em >[possui versão vegana e sem glúten]</em>",
+      anPreco: 32,
+      anFoto: "https://www.instagram.com/p/CR9NOtqrzZP/",
+      boVegetariano: 1,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "MUSHABAK",
+      anDescricao:
+        "massa de semolina frita · calda de especiarias · sorvete de baunilha · nozes · canela`n<em >[possui versão vegana]</em>",
+      anPreco: "29",
+      anFoto: "https://www.instagram.com/p/CQRF32wJJ73/",
+      boVegetariano: 1,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "MÉLI KAI FROÚTA",
+      anDescricao:
+        "iogurte grego · fruta da estação · mel · crocante de massa filo`n<em >[possui versão vegana]</em>",
+      anPreco: "26",
+      anFoto: "",
+      boVegetariano: 1,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+  ],
+  bebidas: [
+    {
+      id: 1,
+      anTitulo: "ÁGUA",
+      anDescricao: "com ou sem gás",
+      anPreco: "7",
+      anFoto: "",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+    {
+      id: "",
+      anTitulo: "ÁGUA SAN PELLEGRINO",
+      anDescricao: "",
+      anPreco: 15,
+      anFoto: "",
+      boVegetariano: 0,
+      boSemGluten: 0,
+      boInactive: 0,
+    },
+    {
+      id: 2,
+      anTitulo: "REFRIGERANTE",
+      anDescricao: "",
+      anPreco: 8,
+      anFoto: "",
+      boVegetariano: 0,
+      boSemGluten: 1,
+      boInactive: 0,
+    },
+  ],
+};
+
+const listaEntradas = jsonCardapioEntradas.entradas;
+const listaContando = listaEntradas.length;
+let listaPrincipais = jsonCardapioEntradas.pratosPrincipais;
+let listaContandoprincipal = listaPrincipais.length;
+let listaSobremesas = jsonCardapioEntradas.sobremesas;
+let listaContandoSobremesas = listaPrincipais.length;
+
+function entradas() {
+  for (i = 0; i <= listaContando; i++) {
+    listaEntradas.forEach((entradas) => {
+      console.log("Nome:", entradas.anTitulo);
+
+      const bodyElement = document.body.insertAdjacentHTML(
+        "beforebegin",
+        `  
+        <p>id:</p>
+        <p>Titulo:${entradas.anTitulo}</p>
+        <p>Descrição: ${entradas.anDescricao}</p>
+        <p>Valor: ${entradas.anPreco}</p>
+        <p>Foto: <a href="${entradas.anFoto}"> ${entradas.anFoto} </a></p>
+        <p> Vegetariano: ${entradas.boVegetariano}</p>
+        <p> Sem Glúten:${entradas.boSemGluten}</p>
+        <p> Inativo: ${entradas.boInactive}</p>
+         `
+      );
+    });
+
+    break;
+  }
+}
+
+function principais() {
+  for (i = 0; i <= listaContandoprincipal; i++) {
+    listaPrincipais.forEach((pratosPrincipais) => {
+      console.log("Nome:", pratosPrincipais.anTitulo);
+
+      const bodyElement = document.body.insertAdjacentHTML(
+        "beforebegin",
+        `
+        <p>id:</p>
+        <p>Titulo:${pratosPrincipais.anTitulo}</p>
+        <p>Descrição: ${pratosPrincipais.anDescricao}</p>
+        <p>Valor: ${pratosPrincipais.anPreco}</p>
+        <p>Foto: <a href="${pratosPrincipais.anFoto}"> ${pratosPrincipais.anFoto} </a></p>
+        <p> Vegetariano: ${pratosPrincipais.boVegetariano}</p>
+        <p> Sem Glúten:${pratosPrincipais.boSemGluten}</p>
+        <p> Inativo: ${pratosPrincipais.boInactive}</p>
+         `
+      );
+    });
+
+    break;
+  }
+}
+
+function sobremesas() {
+  for (i = 0; i <= listaContandoSobremesas; i++) {
+    listaSobremesas.forEach((sobremesas) => {
+      console.log("Nome:", sobremesas.anTitulo);
+
+      const bodyElement = document.body.insertAdjacentHTML(
+        "beforebegin",
+        `
+          <p>id:</p>
+          <p>Titulo:${sobremesas.anTitulo}</p>
+          <p>Descrição: ${sobremesas.anDescricao}</p>
+          <p>Valor: ${sobremesas.anPreco}</p>
+          <p>Foto: <a href="${sobremesas.anFoto}"> ${sobremesas.anFoto} </a></p>
+          <p> Vegetariano: ${sobremesas.boVegetariano}</p>
+          <p> Sem Glúten:${sobremesas.boSemGluten}</p>
+          <p> Inativo: ${sobremesas.boInactive}</p>
+           `
+      );
+    });
+    break;
+  }
+}
